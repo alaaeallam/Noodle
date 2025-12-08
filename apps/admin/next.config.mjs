ce5f7b0a443b310cdcade2b30212f1eb531ed1f8
alaaeallam <alaaeallam@gmail.com>
2025-12-08 17:25:33 +0200

    chore(admin): ignore ESLint during builds

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -3,6 +3,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+  ignoreDuringBuilds: true,
+},
   images: {
     unoptimized: true,
     dangerouslyAllowSVG: true,
